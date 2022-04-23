a8b0aa013162e7a02282292d90f9b2963044fe6c
CarlaCapelli <92063946+CarlaCapelli@users.noreply.github.com>
2022-04-23 15:19:52 -0300

    compra

--- ts/comprar.ts
+++ ts/comprar.ts
@@ -17,6 +17,20 @@ let harina = document.createElement("img");
 harina.src = "harina.jpg";
 harina?.classList.add("fotoProd");
 let fotos: any[] = [mayonesa, cafe, alfajor, harina, yerba, coca];
+let boton1 = document.createElement("button");
+boton1.innerText = "Agregar";
+let boton2 = document.createElement("button");
+boton2.innerText = "Agregar";
+let boton3 = document.createElement("button");
+boton3.innerText = "Agregar";
+let boton4 = document.createElement("button");
+boton4.innerText = "Agregar";
+let boton5 = document.createElement("button");
+boton5.innerText = "Agregar";
+let boton6 = document.createElement("button");
+boton6.innerText = "Agregar";
+let btnComprar = document.getElementById("comprar");
+let botones: any[] = [boton1, boton2, boton3, boton4, boton5, boton6];
 
 let lista: string[] = [
   "Mayonesa",
@@ -31,7 +45,7 @@ let stock: number[] = [15, 10, 40, 22, 13, 18];
 let divElementos = document.getElementById("elementos");
 let divProducto = document.getElementById("productos");
 divProducto?.classList.add("itemProducto");
-function cargarProductos() {
+let cargarProductos = () => {
   for (let i: number = 0; i < lista.length; i++) {
     let divProducto = document.createElement("div");
     divProducto.classList.add("blanco");
@@ -41,18 +55,61 @@ function cargarProductos() {
     nombreProducto.innerHTML = lista[i];
     let precioProducto = document.createElement("p");
     precioProducto.innerHTML = "$" + precio[i];
-
+    let digaCantidad = document.createElement("p");
+    digaCantidad.innerHTML = "Cantidad:";
     let cantidad = document.createElement("input");
+    cantidad.id = "cantidad" + [i];
     cantidad.type = "number";
     cantidad.max = String(stock[i]);
     cantidad.min = "0";
+    let btns = document.createElement("button");
 
+    btns = botones[i];
+
+    divElementos.appendChild(divProducto);
     divProducto.appendChild(imgs);
     divProducto.appendChild(nombreProducto);
     divProducto.appendChild(precioProducto);
+    divProducto.appendChild(digaCantidad);
     divProducto.appendChild(cantidad);
-    divElementos.appendChild(divProducto);
+    divProducto.appendChild(btns);
+  }
+};
+
+function comprarProductos(): void {
+  for (let i: number = 0; i < botones.length; i++) {
+    let suma: number = 0;
+    botones[i].addEventListener("click", function () {
+      let cantidad = document.getElementById("cantidad" + [i]);
+      let cantidadSeleccionada = Number(cantidad.value);
+
+      if (cantidadSeleccionada < 0 || cantidadSeleccionada > stock[i]) {
+        alert("no contamos con la cantidad seleccionada");
+      } else {
+        suma = suma + precio[i] * cantidadSeleccionada;
+        console.log(
+          "agrego " + cantidadSeleccionada + " unidad/es de " + lista[i],
+          "el precio unitario es $" + precio[i]
+        );
+        console.log(" la suma total de " + lista[i] + " es de $" + suma);
+        let divCompra = document.getElementById("compra");
+
+        divCompra.classList.add("naranja");
+
+        let productoCompra = document.createElement("p");
+        productoCompra.innerHTML =
+          lista[i] +
+          " precio unitario:" +
+          precio[i] +
+          " cantidad: " +
+          cantidadSeleccionada +
+          " ,precio total" +
+          suma;
+        divCompra.appendChild(productoCompra);
+      }
+    });
   }
 }
 
 window.onload = cargarProductos();
+window.onload = comprarProductos();
