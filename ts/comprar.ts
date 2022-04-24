43e9c72138eaf0a0c3a22e1245fa46818807cd8c
CarlaCapelli <92063946+CarlaCapelli@users.noreply.github.com>
2022-04-23 23:16:37 -0300

    botonComprar

--- ts/comprar.ts
+++ ts/comprar.ts
@@ -1,20 +1,20 @@
 let alfajor = document.createElement("img");
-alfajor.src = "./alfajor.jpg";
+alfajor.src = "./alfajor.jpeg";
 alfajor?.classList.add("fotoProd");
 let yerba = document.createElement("img");
-yerba.src = "yerba.jpg";
+yerba.src = "yerba.jpeg";
 yerba?.classList.add("fotoProd");
 let mayonesa = document.createElement("img");
-mayonesa.src = "mayonesa.jpg";
+mayonesa.src = "mayonesa.jpeg";
 mayonesa?.classList.add("fotoProd");
 let cafe = document.createElement("img");
-cafe.src = "cafe.jpg";
+cafe.src = "cafe.jpeg";
 cafe?.classList.add("fotoProd");
 let coca = document.createElement("img");
-coca.src = "coca.jpg";
+coca.src = "coca.jpeg";
 coca?.classList.add("fotoProd");
 let harina = document.createElement("img");
-harina.src = "harina.jpg";
+harina.src = "harina.jpeg";
 harina?.classList.add("fotoProd");
 let fotos: any[] = [mayonesa, cafe, alfajor, harina, yerba, coca];
 let boton1 = document.createElement("button");
@@ -29,7 +29,7 @@ let boton5 = document.createElement("button");
 boton5.innerText = "Agregar";
 let boton6 = document.createElement("button");
 boton6.innerText = "Agregar";
-let btnComprar = document.getElementById("comprar");
+
 let botones: any[] = [boton1, boton2, boton3, boton4, boton5, boton6];
 
 let lista: string[] = [
@@ -43,8 +43,7 @@ let lista: string[] = [
 let precio: number[] = [250, 696, 80, 125, 350, 190];
 let stock: number[] = [15, 10, 40, 22, 13, 18];
 let divElementos = document.getElementById("elementos");
-let divProducto = document.getElementById("productos");
-divProducto?.classList.add("itemProducto");
+
 let cargarProductos = () => {
   for (let i: number = 0; i < lista.length; i++) {
     let divProducto = document.createElement("div");
@@ -65,7 +64,18 @@ let cargarProductos = () => {
     let btns = document.createElement("button");
 
     btns = botones[i];
-
+    let btnComprar = document.getElementById("comprar");
+    btnComprar?.addEventListener("click", function () {
+      divElementos.classList.toggle("desaparece");
+      divProducto.classList.toggle("desaparece");
+      nombreProducto.classList.toggle("desaparece");
+      digaCantidad.classList.toggle("desaparece");
+      cantidad.classList.toggle("desaparece");
+      btns.classList.toggle("desaparece");
+      let titu = document.getElementById("titulo");
+      titu?.classList.add("desaparece");
+      btnComprar.classList.add("desaparece");
+    });
     divElementos.appendChild(divProducto);
     divProducto.appendChild(imgs);
     divProducto.appendChild(nombreProducto);
@@ -100,6 +110,7 @@ function comprarProductos(): void {
         divCompra.classList.add("naranja");
 
         let productoCompra = document.createElement("p");
+
         productoCompra.innerHTML =
           "Producto: " +
           lista[i] +
@@ -112,12 +123,19 @@ function comprarProductos(): void {
         sumaTotal = sumaTotal + precio[i] * cantidadSeleccionada;
         descuento = sumaTotal * 0.15;
         totalDescuento = sumaTotal - descuento;
-        console.log(descuento);
-        console.log(totalDescuento);
-        //console.log(sumaTotal);
+
         let divSumaTotal = document.getElementById("sumaTotal");
+        let divSumaTotalDesc = document.getElementById("sumaTotalDescuento");
         divSumaTotal?.innerHTML = "El precio total es de: $" + sumaTotal;
+        divSumaTotalDesc?.innerHTML =
+          "El precio total con descuento es de: $" + totalDescuento;
         divCompra.appendChild(productoCompra);
+        console.log(sumaTotal);
+        if (sumaTotal > 0) {
+          let btnComprar = document.getElementById("comprar");
+
+          btnComprar.classList.remove("desaparece");
+        }
       }
     });
   }
